b655c718fd28075583cfe60a4e001c7197a1d70a
voigtstefan <s.voigt1988@gmx.net>
2017-09-27 17:08:21 +0200

    read.btc can now handle file names and currency/exchange pairs

--- R/get_url.R
+++ R/get_url.R
@@ -2,9 +2,10 @@
 #' @importFrom magrittr '%>%'
 #' @importFrom dplyr filter
 #' @importFrom dplyr select
+
 get.url <- function(exc = "kraken", cur = "USD") {
     url <- files %>% filter(cur == currency, exc == exchange) %>% select(url, names, filename)
-    if (identical(url, character(0))) 
+    if (identical(url, character(0)))
         stop("Currency-Exchange Pair not available")
     return(url)
 }
--- R/read_btc.R
+++ R/read_btc.R
@@ -1,12 +1,31 @@
 #' Read bitcoinchart data
+#' @param exc Exchange
+#' @param cur Fiat money currency
+#' @param file File name which links directly to saved files (use either a exc, cur pair **or** file)
+#' @param folder path to folder where the data is saved
 #' @importFrom readr read_csv
 #' @importFrom dplyr mutate
 #' @export
 
-read.btc <- function(exc = "kraken", cur = "GBP", folder = ".") {
-    url <- get.url(exc, cur)
-    tmp.raw <- read_csv(paste(folder, url$filename, sep = "/"), col_names = c("date", "price", "quantity")) %>% 
-        mutate(date = as.POSIXct(date, origin = "1970-01-01"), currency = sub("([^::A-Z::]+)(.*).csv", "\\2", url$filename), 
-            exchange = sub("([^::A-Z::]+)(.*$)", "\\1", url$filename))
-    return(tmp.raw)
+read.btc <- function(..., folder = ".") {
+    args <- list(...)
+    exist.exccur <- "exc" %in% names(args) && "cur" %in% names(args)
+    exist.file <- "file" %in% names(args)
+    if(exist.exccur){
+        exc<-args$exc
+        cur<-args$cur
+        url <- get.url(exc, cur)
+        tmp.raw <- read_csv(paste(folder, url$filename, sep = "/"), col_names = c("date", "price", "quantity")) %>%
+            mutate(date = as.POSIXct(date, origin = "1970-01-01"), currency = sub("([^::A-Z::]+)(.*).csv", "\\2", url$filename),
+                   exchange = sub("([^::A-Z::]+)(.*$)", "\\1", url$filename))
+        return(tmp.raw)
+    }else if(exist.file){
+        file <- args$file
+        tmp.raw <- read_csv(paste(folder, file, sep = "/"), col_names = c("date", "price", "quantity")) %>%
+            mutate(date = as.POSIXct(date, origin = "1970-01-01"), currency = sub("([^::A-Z::]+)(.*).csv", "\\2", file),
+                   exchange = sub("([^::A-Z::]+)(.*$)", "\\1", file))
+        return(tmp.raw)
+    }else{
+        stop('Wrong input: Provide either Currency // Exchange Pair or File')
+    }
 }
