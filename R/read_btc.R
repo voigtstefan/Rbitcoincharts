8b61ed8a2f1df7ff3afade2eeda13419069bce05
voigtstefan <s.voigt1988@gmx.net>
2017-09-27 08:57:50 +0200

    Update Dependency

--- R/read_btc.R
+++ R/read_btc.R
@@ -5,8 +5,8 @@
 
 read.btc <- function(exc='kraken', cur='GBP', folder='.'){
     url <- get.url(exc, cur)
-    tmp.raw <- read_csv(paste(folder, url$filename, sep='/'), col_names=c('Date', 'Price','Quantity')) %>%
-        mutate(Date = as.POSIXct(Date, origin="1970-01-01"),
+    tmp.raw <- read_csv(paste(folder, url$filename, sep='/'), col_names=c('date', 'price','quantity')) %>%
+        mutate(date = as.POSIXct(date, origin="1970-01-01"),
                currency = sub('([^::A-Z::]+)(.*).csv','\\2',url$filename),
                exchange = sub('([^::A-Z::]+)(.*$)','\\1',url$filename))
     return(tmp.raw)
