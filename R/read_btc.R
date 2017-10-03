67d24a4e3059a70966858bd372b06ed5eadeab64
voigtstefan <s.voigt1988@gmx.net>
2017-10-03 10:06:18 +0200

    Updated read_in col type characters

--- R/read_btc.R
+++ R/read_btc.R
@@ -15,7 +15,7 @@ read.btc <- function(..., folder = ".") {
         exc<-args$exc
         cur<-args$cur
         url <- get.url(exc, cur)
-        tmp.raw <- read_csv(paste(folder, url$filename, sep = "/"), col_names = c("date", "price", "quantity")) %>%
+        tmp.raw <- read_csv(paste(folder, url$filename, sep = "/"), col_names = c("date", "price", "quantity"), col_types = cols()) %>%
             mutate(date = as.POSIXct(date, origin = "1970-01-01"), currency = sub("([^::A-Z::]+)(.*).csv", "\\2", url$filename),
                    exchange = sub("([^::A-Z::]+)(.*$)", "\\1", url$filename))
         return(tmp.raw)
